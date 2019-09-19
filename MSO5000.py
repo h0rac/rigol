ad73fb560573f3fe605d17cc4d53af110f4bb2b6
h0rac <grzegorz.wypych@pl.ibm.com@h0racs-MacBook-Pro.local>
2019-09-19 23:04:01 +0200

    Documentation for rigol class

--- MSO5000.py
+++ MSO5000.py
@@ -2,12 +2,12 @@
 
 from mso.rigol import RigolMSO
         
-mso5000 = RigolMSO("192.168.1.10")
-trigger = mso5000.setTrigger("rs232")
+mso5000 = RigolMSO(addr="192.168.1.10")
+trigger = mso5000.setTriggerType("rs232")
 trigger.setup(src ="chan1", baud=38400, voltage="2.68", when="data", data=10, stopBit=1, parity="none", dataBits=8)
 print(trigger.getCurrentSetup())
 mso5000.sweepTrigger("normal")
-mso5000.enableSystem().output("tout")
+mso5000.getSystem().output("tout")
 rs232Decoder = mso5000.setBus(1, "rs232")
 decoder = mso5000.getLastBus()
 decoder.setup(toggle=True, toggleLabel=True, format="ascii", position=0, threshold="2.68", ttype="tx")
--- mso/rigol.py
+++ mso/rigol.py
@@ -12,8 +12,15 @@ main = None
 with open('{}/rigol/json/main/main.json'.format(os.path.dirname(os.getcwd())), 'r') as f:
     main = json.load(f)
 
-class RigolMSO():        
-    def __init__(self, addr):
+class RigolMSO():
+    """Class represents main rigol device
+
+        Args:
+            addr (str): IP address of rigol device
+        Returns:
+            object: rigol object
+    """       
+    def __init__(self, addr="192.168.1.10"):
         self.instr = vxi11.Instrument(addr)
         print(self.instr.ask("*IDN?"))
         self.triggerType = None
@@ -21,10 +28,20 @@ class RigolMSO():
         self.mainBus = None
         self.bus = None
 
-    def _getTriggerType(self):
+    def getTriggerType(self):
+        """Returns:
+            str: selected trigger type
+        """
         return  self.instr.ask(":TRIGger:MODE?")
 
-    def triggerOn(self, trigger = None):
+    def _triggerOn(self, trigger="rs232"):
+        """Set trigger type
+        Args:
+            trigger (str): pulse|slope|video|pattern|duration|timeout|runt|window|delay|setup
+            |nedge|rs232|iic|spi|can|flexray|lin|iis|m1553
+        Returns:
+            str: trigger type after set
+        """
         if trigger:
             try:
                 self.instr.write(":TRIGger:MODE {0}".format(main['triggers'][trigger.lower()]))
@@ -33,42 +50,70 @@ class RigolMSO():
                 sys.exit()
         else:
             self.instr.write(":TRIGger:MODE {0}".format(self.triggerType))
-        return self._getTriggerType()
+        return self.getTriggerType()
 
-    def getTrigger(self):
-        return self._getTriggerType()
     
     def getAllTriggers(self):
+        """Returns:
+            dict: all possible triggers
+        """
         return main['triggers']
     
-    def setTrigger(self, trigger):
+    def setTriggerType(self, trigger):
+        """Set trigger type
+        Args:
+            trigger (str): pulse|slope|video|pattern|duration|timeout|runt|window|delay|setup
+            |nedge|rs232|iic|spi|can|flexray|lin|iis|m1553
+        Returns:
+            str: trigger type after set
+        """
         try:
             self.triggerType = main['triggers'][trigger.lower()]
-            self.triggerOn(trigger)
+            self._triggerOn(trigger)
             self.trigger = TriggerFactory.factory(trigger.upper(), self.instr )
             return self.trigger
         except KeyError as err:
             print('Unsupported trigger:', err)
             sys.exit()
     
-    def sweepTrigger(self, state):
+    def sweepTrigger(self, mode="auto"):
+        """Set trigger mode
+        Args:
+            mode (str): auto|single|normal
+        """
         try:
-            self.instr.write(":TRIGger:SWEep {0}".format(main['trigger'][state.lower()]))
+            self.instr.write(":TRIGger:SWEep {0}".format(main['trigger'][mode.lower()]))
         except KeyError as err:
             print('Unsupported trigger sweep:', err)
             sys.exit()
 
     def getSweep(self):
+        """Returns:
+            str: trigger mode auto|single|normal
+        """
         return self.instr.ask(":TRIGger:SWEep?")
     
-    def enableSystem(self):
+    def getSystem(self):
+        """Returns:
+            obj: return system object
+        """
         self.system = System(self.instr)
         return self.system
     
     def getLastBus(self):
+        """Returns:
+            str: decoder bus object
+        """
         return self.mainBus
     
     def setBus(self, busNum, busType):
+        """Set bus decoder line
+        Args:
+            busNum (int): line 1|2|3|4
+            busType (str): parallel|rs232|spi|iic|iis|lin|can|flexray|m1553
+        Returns:
+            obj: bus type object
+        """
         self.mainBus = Bus(self.instr)
         try:
             self.mainBus.setBus(int(busNum), busType)
