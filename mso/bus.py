f611fa59cc9397a903985a75f79217feec5e0f22
h0rac <grzegorz.wypych@pl.ibm.com@h0racs-MacBook-Pro.local>
2019-09-19 21:57:32 +0200

    Added documentation

--- mso/bus.py
+++ mso/bus.py
@@ -7,74 +7,124 @@ with open("{}/rigol/json/decoder/bus.json".format(os.path.dirname(os.getcwd())),
     bus = json.load(f)
 
 class BusFactory(object):
+    """Class create decoder objects base on type
+    """
 
     def factory(type, instr, busNum):
+        """Args:
+            instr (obj): vx11 instance
+            busNum (int): decoder bus number
+        Returns:
+            object: decoder object base on type
+        """
         if type == "RS232":
             return RS232Bus(instr, busNum)
-
     factory = staticmethod(factory)
 
 class Bus():
+    """Decoder bus class
+
+        Args:
+            instr (obj): vx11 instance
+
+        Returns:
+            object: main decoder object
+    """
     def __init__(self, instr):
         self.instr = instr
         self.busNum = None
         self.ttype = "tx"
     
     def getMode(self):
+        """Returns:
+            str: selcted decoder bus mode
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:MODE?".format(self.busNum))
     
     def getBusStatus(self):
+        """Returns:
+            str: 0|1 indicate ON/OFF for decoder bus
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:DISPlay?".format(self.busNum))
     
     def getBusDataFormat(self):
+        """Returns:
+            str: data display format for bus
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:FORMat?".format(self.busNum))
     
     def getBusLabelStatus(self):
+        """Returns:
+            str: 0|1 indicate ON/OFF for decoder bus label
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:LABel?".format(self.busNum))
     
     def getEventTableStatus(self):
+        """Returns:
+            str: 0|1 indicate ON/OFF for decoder event table
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:EVENt?".format(self.busNum))
     
     def getEventTableFormat(self):
+        """Returns:
+            str: format for event table data
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:EVENt:FORMat?".format(self.busNum))
     
     def getEventTableView(self):
+        """Returns:
+            str: event table view
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:EVENt:VIEW?".format(self.busNum))
     
     def getPosition(self):
+        """Returns:
+            str: current position of decoder bus on screen
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:POSition?".format(self.busNum))
      
     def getThreshold(self, ttype="tx"):
+        """
+        Args:
+            ttype (str): line for which threshold should be enabled
+        Returns:
+            str: threshold value
+        """
         if not self.busNum:
             print("First bus type and number need to be set")
             sys.exit(0)
         return self.instr.ask(":BUS{0}:THReshold? {1}".format(self.busNum, ttype))
     
-    def setBus(self, busNum, busType):
+    def setBus(self, busNum=1, busType="rs232"):
+        """Get bus number and type and set decoder bus
+        Args:
+            busNum (int): bus number 1|2|3|4
+            busType (str): parallel|rs232|spi|iic|iis|lib|can|flexray|m1553
+        """
         if busNum > 0 and busNum <= 4:
             self.busNum = busNum
             self.instr.write(":BUS{0}:MODE {1}".format(str(busNum), bus['mode'][busType.lower()]))
@@ -82,17 +132,25 @@ class Bus():
             print("Device support only 4 buses")
             sys.exit()
     
-    def toggle(self, status=False):
-        if status:
+    def toggle(self, toggle=False):
+        """Enable bus decoder base on status
+        Args:
+            toggle (bool): True|False
+        """
+        if toggle:
             if self.busNum > 0 and self.busNum <= 4:
                 self.instr.write(":BUS{0}:DISPlay ON".format(self.busNum))
-        elif not status:
+        elif not toggle:
             if self.busNum > 0 and self.busNum <= 4:
                 self.instr.write(":BUS{0}:DISPlay OFF".format(self.busNum))
         else:
             print("Expect 'True' or 'False' to toggle bus'")
     
     def toggleLabel(self, toggle=False):
+        """Enable bus decoder label base on toggle 
+        Args:
+            toggle (bool): True|False
+        """
         if toggle == True:
             if self.busNum > 0 and self.busNum <= 4:
                 self.instr.write(":BUS{0}:LABel ON".format(self.busNum))
@@ -103,12 +161,20 @@ class Bus():
             print("Expect 'True' or 'False to toggle bus label'")
     
     def setDataformat(self, format):
+        """Enable bus decoder data format
+        Args:
+            format (str): ascii|hex|dec|bin
+        """
         try:
             self.instr.write(":BUS{0}:FORMat {1}".format(self.busNum, bus['format'][format.lower()]))
         except KeyError as err:
             print("Unsupported data format: ", err)
     
     def setPosition(self, pos):
+        """Set bus decoder position
+        Args:
+            pos (int): pos >= -167 and pos <=217
+        """
         if self.busNum > 0 and self.busNum <= 4:
             if pos >=-167 and pos <=217:
                  self.instr.write(":BUS{0}:POSition {1}".format(self.busNum, pos))
@@ -118,6 +184,11 @@ class Bus():
             print("Device support only 4 buses")
     
     def setThreshold(self, threshold, ttype):
+        """Set bus decoder threshold value to proper line type
+        Args:
+            threshold (str): value that represents voltage level, accept extension kV|uV|mV|V
+            ttype (str): pal|tx|rx|scl|sda|cs|clk|miso|mosi|lin|can|cansub1
+        """
         if self.busNum > 0 and self.busNum <= 4:
             self.ttype = ttype
             self.instr.write(":BUS{0}:THReshold {1},{2}".format(self.busNum, threshold, bus['threshold'][ttype.lower()]))
@@ -125,13 +196,25 @@ class Bus():
             print("Device support only 4 buses")
     
     def setup(self, toggle=False, toggleLabel=False, format="ascii", position=0, threshold=0.0, ttype="tx"):
+        """Set bus decoder in single function
+        Args:
+            toggle (bool): enable bus decoder
+            toggleLabel (bool): enable bus decoder label display
+            format (str): defines format data to display
+            position (str): defines position of decoder line on screen
+            threshold (str): voltage level 
+            ttype (str): type of line
+        """
         self.toggle(toggle)
         self.toggleLabel(toggleLabel)
         self.setDataformat(format)
         self.setPosition(position)
         self.setThreshold(threshold, ttype)
     
-    def getCurrentSetup(self, ):
+    def getCurrentSetup(self):
+        """Returns:
+            dict: dictionary of bus decoder setup represented by key,value pair
+        """
         return {"mode":self.getMode(), "display":self.getBusStatus(), "format":self.getBusDataFormat(), 
         "toggleLabel":self.getBusLabelStatus(),
         "position":self.getPosition(),
@@ -144,6 +227,12 @@ class Bus():
         }
     
     def toggleEventTable(self, toggle=False, format="ascii", view="packets"):
+        """Set bus decoder event table
+        Args:
+            toggle (bool): enable or disable event table on screen
+            format (str): data format to display for event table
+            view (str): default view for event table
+        """
         if self.busNum > 0 and self.busNum <= 4:
             if toggle == True:
                 self.instr.write(":BUS{0}:EVENt ON".format(self.busNum))
@@ -159,13 +248,20 @@ class Bus():
 
 
 class RS232Bus(Bus):
+    """Class represents rs232 decoder
+
+        Args:
+            instr (obj): vx11 instance
+            busNum (int): decoder bus number
+        Returns:
+            object: rs232 decoder object
+    """
     def __init__(self, instr, busNum):
         self.instr = instr
         self.busNum = busNum
     
     def setRXSource(self, src):
         pass
-
     
     def setBoud(self):
         print("Boud set", self.busNum)
--- mso/triggers.py
+++ mso/triggers.py
@@ -6,90 +6,178 @@ with open("{}/rigol/json/trigger/rs232.json".format(os.path.dirname(os.getcwd())
     rs232 = json.load(f)
 
 class TriggerFactory(object):
+    """Produces trigger objects"""
 
     def factory(type, instr):
+        """Produce object base on trigger type
+
+        Args:
+            type (str): Type of trigger
+            instr (obj): vx11 instance
+
+        Returns:
+            object: Trigger object base on type
+        """
         if type == "RS232":
             return RS232(instr)
 
     factory = staticmethod(factory)
 
 class RS232(TriggerFactory):
+    """RS232 Trigger class"""
 
     def __init__(self, instr):
+        """
+            Args:
+                instr (obj): vx11 instance
+            
+            Returns:
+            object: RS232 trigger object   
+        """
         TriggerFactory.__init__(self)
         self.instr = instr
 
     def getSourceChannel(self):
-          return  self.instr.ask(":TRIGger:RS232:SOURce?")
+        """Returns:
+            str: source trigger channel
+        """
+        return  self.instr.ask(":TRIGger:RS232:SOURce?")
 
     def getBoud(self):
+        """Returns:
+            str: boud rate
+        """
         return  self.instr.ask(":TRIGger:RS232:BAUD?")
     
     def getVoltageLevel(self):
+        """Returns:
+            str: voltage level
+        """
         return  self.instr.ask(":TRIGger:RS232:LEVel?")
 
     def getWhen(self):
-         return self.instr.ask(":TRIGger:RS232:WHEN?")
+        """Returns:
+            str: when trigger will happen
+        """
+        return self.instr.ask(":TRIGger:RS232:WHEN?")
     
     def getData(self):
+        """Returns:
+            str: if when is set to 'DATA' return trigger data in decimal 0-255
+        """
         return  self.instr.ask(":TRIGger:RS232:DATA?")
     
     def getStopBit(self):
-         return self.instr.ask(":TRIGger:RS232:STOP?")
+        """Returns:
+            str: stop bit for rs232 with values 1|1,5|2
+        """
+        return self.instr.ask(":TRIGger:RS232:STOP?")
     
     def getParity(self):
+        """Returns:
+            str: parity for rs232 with values None|Odd|Even
+        """
         return  self.instr.ask(":TRIGger:RS232:PARity?")
     
     def getDataBits(self):
+        """Returns:
+            str: data bits for rs232 with values 5|7|6|8
+        """
         return  self.instr.ask(":TRIGger:RS232:WIDTh?")
 
-    def setBoud(self, boud):
+    def setBoud(self, boud=9600):
+        """Set boud lever for trigger
+
+        Args:
+            boud (str): baud rate for rs232
+            If the baud rate is set to a value with "M", 
+            then "A" should be added at the end of the value. For example, if you send 5 M, you need to send 5 MA
+        """
         try:
             self.instr.write(":TRIGger:RS232:BAUD {0}".format(boud))
-        except KeyError as err:
+        except ValueError as err:
             print('Unsupported boud:', err)
     
-    def setVoltageLevel(self, voltage):
+    def setVoltageLevel(self, voltage=0.0):
+        """Set voltage lever for trigger
+
+        Args:
+            voltage (str): support extension with mV|uV|nV|kV|V
+        """
         try:
             self.instr.write(":TRIGger:RS232:LEVel {0}".format(voltage))
-        except Error as err:
+        except ValueError as err:
             print('Unsupported voltage:', err)
             sys.exit()
     
-    def setSourceChannel(self, chan):
+    def setSourceChannel(self, chan="chan1"):
+        """Set channel source for trigger
+
+        Args:
+            chan (str): source channel for trigger
+            provide as chan1, chan2... it will be convered to CHANnel1, CHANnel2 etc 
+            
+            available channels:
+            D0|D1|D2|D3|D4|D5|D6|D7|D8|D9|D1 0|D11|D12|D13|D14|D15|CHANnel1|CHANnel2|CHANnel3|CHANnel4
+        """
         try:
             self.instr.write(":TRIGger:RS232:SOURce {0}".format(rs232['source'][chan.lower()]))
         except KeyError as err:
             print('Unsupported channel source:', err)
             sys.exit()
         
-    def setWhen(self, value):
+    def setWhen(self, value="data"):
+        """Set when trigger fire
+
+        Args:
+            when (str): start|error|cerror|data
+        """
         try:
             self.instr.write(":TRIGger:RS232:WHEN {0}".format(rs232['when'][value.lower()]))
         except KeyError as err:
             print('Unsupported when source:', err)
     
-    def setData(self, num):
+    def setData(self, num=10):
+        """Set on which ASCII char trigger when "data" is set as when
+
+        Args:
+            num (int): decimal 0-255
+        """
         result = num in rs232['data']
         if result:
             self.instr.write(":TRIGger:RS232:DATA {0}".format(num))
         else:
             print('Unsupported data, accept value in range 0-255')
     
-    def setStopBit(self, stopBit):
+    def setStopBit(self, stopBit=1):
+        """Set stop bit for rs232
+
+        Args:
+            stopBit (int): accepts 1|1.5|2
+        """
         result = stopBit in rs232['stopBits']
         if result:
             self.instr.write(":TRIGger:RS232:STOP {0}".format(stopBit))
         else:
             print('Unsupported value, accept 1,1.5,2')
     
-    def setParity(self, parity):
+    def setParity(self, parity="none"):
+        """Set stop bit for rs232
+
+        Args:
+            parity (str): accepts none|even|odd
+        """
         try:
             self.instr.write(":TRIGger:RS232:PARity {0}".format(rs232['parity'][parity.lower()]))
         except KeyError as err:
             print('Unsupported parity, accept ODD, EVEN, NONE:', err)
     
-    def setDataBits(self, bits):
+    def setDataBits(self, bits=8):
+        """Set stop bit for rs232
+
+        Args:
+            bits (int): accepts 5|6|7|8
+        """
         result = bits in rs232['dataBits']
         if result:
             self.instr.write(":TRIGger:RS232:WIDTh {0}".format(bits))
@@ -97,7 +185,19 @@ class RS232(TriggerFactory):
             print('Unsupported value, accept 5,6,7 or 8')
          
     
-    def setup(self, src = "chan1", voltage = 0, baud=9600, when="data",data=10, dataBits=8, stopBit=1, parity="none"):
+    def setup(self, src = "chan1", voltage = 0.0, baud=9600, when="data",data=10, dataBits=8, stopBit=1, parity="none"):
+        """Set rs232 parameters in one function
+
+        Args:
+            src (str): source channel
+            voltage (str): source voltage
+            boud (str): boud rate
+            when (str): when to trigger
+            data (str): if when is data set ASCII dec value to trigger
+            dataBits (int): data bits
+            stopBit (int): stop bit
+            parity (str): parity for rs232
+        """
         self.setSourceChannel(src)
         self.setVoltageLevel(voltage)
         self.setBoud(baud)
@@ -109,16 +209,27 @@ class RS232(TriggerFactory):
         self.setDataBits(dataBits)
     
     def restoreDefault(self):
+        """Restore rs232 trigger options to default
+            
+        Returns:
+            dict: with default key,pair values
+        """
         self.setup()
         return {'src':self.getSourceChannel(), 'voltage':self.getVoltageLevel(), 'baud':self.getBoud(), "when":self.getWhen(),
         'data':self.getData(), "stopBit":self.getStopBit()
          }
 
     def getDefaults(self):
+        """Returns:
+            dict: with availble default key,pair values
+        """
         return {'src':'chan1', 'voltage':0, 'baud':9600, 'when':"data","data":10, "dataBits":8, "stopBit":1, "parity":'none'}
     
     def getCurrentSetup(self):
+        """Returns:
+            dict: current configuration
+        """
         return {'src':self.getSourceChannel(), 'voltage':self.getVoltageLevel(), 'baud':self.getBoud(), 
-        'when':self.getWhen(), "dataBits":self.getDataBits(), "stopBit":self.getStopBit(), "parity":self.getParity()}
+        'when':self.getWhen(), 'data':self.getData(), "dataBits":self.getDataBits(), "stopBit":self.getStopBit(), "parity":self.getParity()}
 
 
